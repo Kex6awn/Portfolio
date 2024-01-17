977ef4f8d549eefa617463b528d07ab8627c8cfd
Kex6awn <fferdinard1@gmail.com>
2024-01-17 11:46:20 -0600

    javascript changes

--- testing.js
+++ testing.js
@@ -3,3 +3,13 @@ Name:       Francis Ferdinard
 Date:       1/17/2024
 */
 
+let newVariable = 9;
+let secondVariable = 10;
+
+let total = newVariable + secondVariable;
+
+console.log(total);
+
+let questionsAndAnswers = prompt("What is your favorite food?");
+
+console.log("The user entered" + " " + questionsAndAnswers);
