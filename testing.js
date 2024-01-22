429b9153138e7da06428d15a6821bc82ad4c688d
Kex6awn <fferdinard1@gmail.com>
2024-01-22 12:06:00 -0600

    Assignment #2

--- testing.js
+++ testing.js
@@ -13,3 +13,22 @@ console.log(total);
 let questionsAndAnswers = prompt("What is your favorite food?");
 
 console.log("The user entered" + " " + questionsAndAnswers);
+
+
+//Assignment #2
+
+// For Loop, If statement, and increment
+/*
+Name:       Francis Ferdinard
+Date:       1/22/2024
+*/
+
+let numberVariable = 5;
+
+for ( i = 0; i < 5; i++) {
+    numberVariable++;
+    
+    if (numberVariable % 2 != 0) {
+        console.log(numberVariable);
+    }
+}
