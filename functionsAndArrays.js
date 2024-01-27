1b318459a93f98773e2cbd7f3dbaf13208f1f430
Kex6awn <fferdinard1@gmail.com>
2024-01-27 13:38:12 -0600

    update

--- functionsAndArrays.js
+++ functionsAndArrays.js
@@ -1,11 +1,15 @@
-let argument = [5, 10, 15, 20];
-let myInt = 10;
-let myInt2 = 13;
 
 /*
 Name:           Francis Ferdinard
 Date:           1/25/2024
 */
+
+/*
+
+let argument = [5, 10, 15, 20];
+let myInt = 10;
+let myInt2 = 13;
+
 
 let firstFunction = function(singleParameter) {
     singleParameter += 1;
@@ -28,3 +32,29 @@ function newFunction(func, secondParameter) {
 for (let x in argument) {
     newFunction(firstFunction, argument[x]);
 }
+
+*/
+
+//
+let createAnArray = [5, 10, 15, 20, 25, 30];
+//let aNumber = 6;
+
+let functionExpression = function(parameterOne) {
+    parameterOne += 1;
+    console.log(parameterOne);
+}
+
+function myFunction(functionExpression, aNumber) {
+    if (aNumber % 2 == 0) {
+        functionExpression = aNunber;
+    }
+    else {
+        console.log('The number is odd');
+    }
+}
+
+for (let i in createAnArray) {
+    myFunction(functionExpression(i));
+}
+
+//
