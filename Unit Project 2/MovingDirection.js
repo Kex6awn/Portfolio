3bd7a4eadda46a89fd8e40ea36ab8920ec9af620
Kex6awn <fferdinard1@gmail.com>
2024-03-26 03:56:37 -0500

    Direction

--- Unit Project 2/MovingDirection.js
+++ Unit Project 2/MovingDirection.js
@@ -1,6 +1,80 @@
 const canvas = document.getElementById("gameArea");
 const ctx = canvas.getContext("2d");
 
+class Object {
+    
+    constructor(x, y, radius, speed, canvas) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speed = speed;
+        this.canvas = canvas;
+    }
+
+    boundryCheck() {
+          //up
+        if (y < radius) {
+            y = radius;
+        }
+        //down
+        if (y > canvas.height - radius) {
+            y = canvas.height - radius;
+        }
+        //left
+        if (x < radius) {
+            x = radius;
+        }
+        //right
+        if (x > canvas.width - radius) {
+            x = canvas.width - radius;
+        }
+    }
+
+    inputs() {
+        if (upPressed) {
+          y = y - speed;
+        }
+        if (downPressed) {
+          y = y + speed;
+        }
+        if (leftPressed) {
+          x = x - speed;
+        }
+        if (rightPressed) {
+          x = x + speed;
+        }
+    }
+
+    drawGreenBlob() {
+        ctx.fillStyle = "green";
+        if (upPressed) {
+          ctx.fillStyle = "red";
+        }
+        if (downPressed) {
+          ctx.fillStyle = "blue";
+        }
+        if (leftPressed) {
+          ctx.fillStyle = "yellow";
+        }
+        if (rightPressed) {
+          ctx.fillStyle = "purple";
+        }
+      
+        ctx.beginPath();
+        ctx.arc(x, y, radius, 0, Math.PI * 2);
+        ctx.fill();
+    }
+
+    // clearScreen() {
+    //     ctx.fillStyle = "black";
+    //     ctx.fillRect(0, 0, canvas.width, canvas.height);
+    // }
+
+
+}
+
+const visual = new Object(100, 100, 50, 10, canvas);
+
 let x = 100;
 let y = 100;
 let radius = 50;
@@ -14,44 +88,48 @@ let rightPressed = false;
 function drawGame() {
   requestAnimationFrame(drawGame);
   clearScreen();
-  inputs();
-  boundryCheck();
+  visual.inputs();
+  visual.boundryCheck();
   drawGreenBlob();
 }
 
-function boundryCheck() {
-  //up
-  if (y < radius) {
-    y = radius;
-  }
-  //down
-  if (y > canvas.height - radius) {
-    y = canvas.height - radius;
-  }
-  //left
-  if (x < radius) {
-    x = radius;
-  }
-  //right
-  if (x > canvas.width - radius) {
-    x = canvas.width - radius;
-  }
-}
 
-function inputs() {
-  if (upPressed) {
-    y = y - speed;
-  }
-  if (downPressed) {
-    y = y + speed;
-  }
-  if (leftPressed) {
-    x = x - speed;
-  }
-  if (rightPressed) {
-    x = x + speed;
-  }
-}
+
+// function boundryCheck() {
+//   //up
+//   if (y < radius) {
+//     y = radius;
+//   }
+//   //down
+//   if (y > canvas.height - radius) {
+//     y = canvas.height - radius;
+//   }
+//   //left
+//   if (x < radius) {
+//     x = radius;
+//   }
+//   //right
+//   if (x > canvas.width - radius) {
+//     x = canvas.width - radius;
+//   }
+// }
+
+
+
+// function inputs() {
+//   if (upPressed) {
+//     y = y - speed;
+//   }
+//   if (downPressed) {
+//     y = y + speed;
+//   }
+//   if (leftPressed) {
+//     x = x - speed;
+//   }
+//   if (rightPressed) {
+//     x = x + speed;
+//   }
+// }
 
 function drawGreenBlob() {
   ctx.fillStyle = "green";
@@ -69,7 +147,7 @@ function drawGreenBlob() {
   }
 
   ctx.beginPath();
-  ctx.arc(x, y, radius, 0, Math.PI * 2);
+  ctx.arc(visual.drawGreenBlob(), 0, Math.PI * 2);
   ctx.fill();
 }
 
